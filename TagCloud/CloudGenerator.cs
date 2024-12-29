c50584fce35018b5842c6c7a11910df4af427eed
vafle228 <dimasmyslaev228@gmail.com>
2024-12-29 19:20:13 +0500

    Переписал генератор облаков на резалт

--- TagCloud/CloudGenerator.cs
+++ TagCloud/CloudGenerator.cs
@@ -1,10 +1,12 @@
-﻿using TagCloud.ImageGenerator;
+﻿using FuncTools;
+using TagCloud.ImageGenerator;
 using TagCloud.ImageSaver;
 using TagCloud.WordsFilter;
 using TagCloud.WordsReader;
 
 namespace TagCloud;
 
+#pragma warning disable CA1416
 public class CloudGenerator(
     IImageSaver saver,
     IWordsReader reader, 
@@ -14,21 +16,26 @@ public class CloudGenerator(
     private const int MIN_FONT_SIZE = 10;
     private const int MAX_FONT_SIZE = 80;
 
-    public string GenerateTagCloud()
-    {
-        var words = reader.ReadWords();
-
-        var freqDict = filters
-            .Aggregate(words, (c, f) => f.ApplyFilter(c))
+    public Result<string> GenerateTagCloud() 
+        => reader
+            .ReadWords()
+            .Then(BuildFreqDict)
+            .Then(ToWordTagList)
+            .Then(imageGenerator.GenerateWindowsBitmap)
+            .Then(saver.Save);
+    
+    private static Result<List<WordTag>> ToWordTagList(Dictionary<string, int> freqDict)
+        => freqDict.Values.Max().AsResult().Then(
+            m => freqDict.Select(p => ToWordTag(p, m)).ToList());
+    
+    private Result<Dictionary<string, int>> BuildFreqDict(List<string> words) 
+        => ApplyFilters(words).Then(wl => wl
             .GroupBy(w => w)
             .OrderByDescending(g => g.Count())
-            .ToDictionary(g => g.Key, g => g.Count());
-        
-        var maxFreq = freqDict.Values.Max();
-        var tagsList = freqDict.Select(pair => ToWordTag(pair, maxFreq)).ToList();
-        
-        return saver.Save(imageGenerator.GenerateWindowsBitmap(tagsList));
-    }
+            .ToDictionary(g => g.Key, g => g.Count()));
+
+    private Result<List<string>> ApplyFilters(List<string> words)
+        => filters.Aggregate(words.AsResult(), (c, f) => c.Then(f.ApplyFilter));
 
     private static int TransformFreqToSize(int freq, int maxFreq) 
         => (int)(MIN_FONT_SIZE + (float)freq / maxFreq * (MAX_FONT_SIZE - MIN_FONT_SIZE));
@@ -36,3 +43,4 @@ public class CloudGenerator(
     private static WordTag ToWordTag(KeyValuePair<string, int> pair, int maxFreq)
         => new(pair.Key, TransformFreqToSize(pair.Value, maxFreq));
 }
+#pragma warning restore CA1416
